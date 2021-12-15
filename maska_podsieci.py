517b28cc51302bc5743bce5199ca84fa26f391a3
Pawel <poczta130@gmail.com>
2021-12-15 11:57:53 +0000

    Create maska_podsieci.py

---
+++ maska_podsieci.py
@@ -0,0 +1,31 @@
+def host_count(maska):
+    mlist = maska.split('.')
+    zeros = 0
+    for octet in mlist:
+        octint = int(octet)
+        if octint == 0:
+            zeros += 8
+        elif (octint > 0) and (octint < 255):
+            octbin = str(bin(octint))
+            zeros += octbin.count('0')-1 # Minus jedno zero, bo funkcja bin() zwraca liczbe z prefixem 0b np. 0b11110000
+    return 2**zeros - 2
+  
+
+maskvals = ['0','128','192','224','240','248','252','254']
+
+for maskval in maskvals:
+    maska = maskval+'.0.0.0'
+    hosty = host_count(maska)
+    print(' '*(15-len(maska)),maska,'=',hosty)
+for maskval in maskvals:
+    maska = '255.'+maskval+'.0.0'
+    hosty = host_count(maska)
+    print(' '*(15-len(maska)),maska,'=',hosty)
+for maskval in maskvals:
+    maska = '255.255.'+maskval+'.0'
+    hosty = host_count(maska)
+    print(' '*(15-len(maska)),maska,'=',hosty)
+for maskval in maskvals:
+    maska = '255.255.255.'+maskval
+    hosty = host_count(maska)
+    print(' '*(15-len(maska)),maska,'=',hosty
