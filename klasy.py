8fcc30af6820b022a3f853b46202f59779ffd60a
Pawel <poczta130@gmail.com>
2021-12-15 14:17:40 +0000

    zmiany

--- klasy.py
+++ klasy.py
@@ -35,13 +35,48 @@ class Uczen:
 uczen1 = Uczen("Ania", "Do", 8)
 uczen2 = Uczen("Kamil", "Uch", 7)
 
-print(uczen1.name)
-print(uczen2.name)
-
-
-print(uczen1.show_full_name())
-print(uczen2.show_full_name())
+# print(uczen1.name)
+# print(uczen2.name)
+#
+#
+# print(uczen1.show_full_name())
+# print(uczen2.show_full_name())
 
 
 #stworzyc klase prostokat, ktory w inicie bedzie dwa parametry, dlugosci i szerokosc oraz metoda pole,
 # ktora wzraca pole prostokata, i na koncu stworzyc
+
+class prostokat:
+
+    #wlasciwosci
+    def __init__(self, bok_a, bok_b):
+        self.bok_a = bok_a
+        self.bok_b = bok_b
+
+    #metody
+    def pole_prostokat(self):
+        return self.bok_a*self.bok_b
+
+    @staticmethod
+    def pole_prostokat2(x, y):
+        return x*y
+
+    #magic/special functions
+    def __str__(self):
+        return "To jest prostokat o bokach dlugosci a={}, b={}".format(self.bok_a, self.bok_b)
+
+    def __add__(self, other):
+        return self.bok_a+other.bok_a, self.bok_b+other.bok_b
+
+
+p1 = prostokat(2,7)
+print(p1)
+
+p2 = prostokat(8,9)
+print(p2)
+
+p3 = prostokat(8,9)
+print(p2)
+
+# prostokat.pole_prostokat2(7,8)
+
