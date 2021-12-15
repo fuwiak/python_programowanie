cc55c0f6cabbc9281ee82359c8bcdad0cee21e39
Pawel <poczta130@gmail.com>
2021-12-15 12:27:01 +0000

    Update wyjatki.py

--- wyjatki.py
+++ wyjatki.py
@@ -14,3 +14,6 @@ for i in range(len(log)):
     except Exception as e:
         #czynnosci, ktore wykonujemy w razie awarii
         print(e)
+
+#zadanie wyciagnac dla kazdej linia IP, i drukowac tylko wiersze zaczynajacde od ip 127
+#dla ip ktore nie zaczynaja od 127, dac komunikat "access denied", regex i wykj
