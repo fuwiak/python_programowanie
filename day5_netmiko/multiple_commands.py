3a37ea87cb96219ac80c2bd830cc8b26f03437d0
Pawel <poczta130@gmail.com>
2021-12-17 14:36:32 +0100

    Update multiple_commands.py

--- day5_netmiko/multiple_commands.py
+++ day5_netmiko/multiple_commands.py
@@ -11,7 +11,7 @@ def send_show_command(device, commands):
     result = {}
     try:
         with ConnectHandler(**device) as ssh:
-            ssh.enable()
+            # ssh.enable()
             for command in commands:
                 output = ssh.send_command(command)
                 result[command] = output
@@ -19,14 +19,28 @@ def send_show_command(device, commands):
     except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
         print(error)
 
+lista_ouput = []
 
-if __name__ == "__main__":
-    device = {
-        "device_type": "cisco_ios_telnet",
-        "host": "192.168.100.1",
-        "username": "cisco",
-        "password": "cisco",
-        "secret": "cisco",
-    }
-    result = send_show_command(device, ["sh clock", "sh ip int br"])
-    pprint(result, width=120)
+
+linux_router = {
+    'device_type': 'linux',
+    'host': '192.168.43.197',
+    'username': 'pawel',
+    'password': 'pawel',
+
+}
+result = send_show_command(linux_router, ["ifconfig", "ip a"])
+pprint(result, width=120)
+lista_ouput.append(result)
+
+#zapisac wynik z lista_output do plik txt, uzywajac funck, jako argument to nazwa do pliku do ktorej
+#bedziemy zapisywac output
+
+# with open('myfile.txt', 'w') as f: #write, 'r' - czytanie, 'a' - dodajemy linie do pliku
+#     print(slownik, file=f)
+#
+#
+# with open(r'plik.txt', 'w' ) as plik:
+#     for klucz, wartosc in slownik.items():
+#         wiersz = '{}={}\n'.format(klucz,wartosc)
+#         plik.write(wiersz)
