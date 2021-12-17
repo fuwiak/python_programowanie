00ccf23b7cbc26945f73dd13031667805620ab88
Pawel <poczta130@gmail.com>
2021-12-17 10:08:49 +0100

    Update grupa_zadania.py

--- day5_db/grupa_zadania.py
+++ day5_db/grupa_zadania.py
@@ -1,4 +1,5 @@
 import psycopg2
+import random
 from psycopg2 import Error
 
 
@@ -10,21 +11,56 @@ connection = psycopg2.connect(user="postgres",
                                   database="postgres")
 
 
+# cursor = connection.cursor()
+# create_table_query = '''CREATE TABLE mobile2
+#           (ID INT PRIMARY KEY     NOT NULL,
+#           MODEL           TEXT    NOT NULL,
+#           PRICE         REAL); '''
+# # Execute a command: this creates a new table
+# cursor.execute(create_table_query)
+# connection.commit()
+# print("Table created successfully in PostgreSQL ")
+#
+#
+# # Executing a SQL query to insert data into  table
+#
+#
+# for i in range(5):
+#     id, model, price = i, "xiaomi"+str(i), 1000+20*i
+#     insert_query = """ INSERT INTO mobile2 (ID, MODEL, PRICE) VALUES ({}, {}, {})""".format(id, model, price)
+#     cursor.execute(insert_query)
+#     connection.commit()
+#
+# cursor.close()
+# connection.close()
+#
+
+
+#zadanie bojowe, uzywajac np petli, dodac do bazy mobile2 5 rzonych modeli telefonow oraz losowe ceny
+#random xiami
+
+#lukasz
+# connection = psycopg2.connect(**conn_data)
 cursor = connection.cursor()
-create_table_query = '''CREATE TABLE mobile2
+create_table_query = '''CREATE TABLE IF NOT EXISTS mobile2
           (ID INT PRIMARY KEY     NOT NULL,
           MODEL           TEXT    NOT NULL,
           PRICE         REAL); '''
-# Execute a command: this creates a new table
+
 cursor.execute(create_table_query)
 connection.commit()
 print("Table created successfully in PostgreSQL ")
 
+mobilki=["LDŻ","Szajsung","Hujej","japko","alcatel"]
 
 # Executing a SQL query to insert data into  table
-insert_query = """ INSERT INTO mobile2 (ID, MODEL, PRICE) VALUES (1, 'Iphone12', 1100)"""
-cursor.execute(insert_query)
+i = 1
+for element in mobilki:
+    print(element);
+    insert_query = """INSERT INTO mobile2 (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
+    insert_v = (i, element, random.randint(1000,5000))
+    i += 1
+    print(insert_v)
+    cursor.execute(insert_query, insert_v)
 connection.commit()
 
-cursor.close()
-connection.close()
