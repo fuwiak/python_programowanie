e5338e11203d3e9dd2f2da06262b0d94d7b28326
Pawel <poczta130@gmail.com>
2022-01-17 14:49:51 +0100

    Add files via upload

--- day1/day1.py
+++ day1/day1.py
@@ -98,22 +98,22 @@ a, b, c = 7, 1, 1
 # a=1
 # b=2
 # c=1
-delta = b**2-4*a*c
-
-print("delta", delta)
-if delta>0:
-    print("2 rozwiazania")
-elif delta==0:
-    print("1 rozwiazanie")
-elif delta==2:
-    print("delta rowna 2")
-else:
-    print("Brak rozwiazan")
+# delta = b**2-4*a*c
+#
+# print("delta", delta)
+# if delta>0:
+#     print("2 rozwiazania")
+# elif delta==0:
+#     print("1 rozwiazanie")
+# elif delta==2:
+#     print("delta rowna 2")
+# else:
+#     print("Brak rozwiazan")
 
 
 #zad bmi
- waga = input()
- wzrost = input()
+ # waga = input()
+ # wzrost = input()
 
 
 # bmi=waga/wzrost**2
@@ -123,7 +123,156 @@ else:
 #na postawie danych z wikipedii, wypisywac komunikaty w stulu - wychudzony, otyly itd
 #zakresy bmi znajduja sie na wikipedii(link w czacie)
 
+# waga = float(input("Wpisz swoja wage"))
+# wzrost = float(input("Wpisz swoj wzrost w metrach"))
+# bmi = waga/wzrost**2
+#
+# print("Twoje BMI wynosi", bmi, "Twoja kategoria bmi to:\n")
+# if bmi < 16:
+#     print("wyglodzenie")
+# elif bmi >= 16 and bmi <16.99:
+#     print("Wychudzenie")
+# elif bmi >= 17 and bmi < 18.49:
+#     print("Niedowaga")
+# elif bmi >= 18.5 and bmi <24.99:
+#     print("Pozadana masa ciala")
+# else:
+#     print("idz na silownie")
 
-if bmi>=16 and bmi<16.99:
-    print("wychudzenie")
+
+
+
+# for i in range(1,11): #wypisz wsystkie liczby z zakresu od 1 do 11(ale juz 11 nie uwzgledniaj)
+#     print(i)
+
+
+
+# for i in range(6, 100, 6): #start 2, end = 100(bez 100), krok = 5
+    # print("Wartość iteratora to " + str(i))
+    # print("Wartość iteratora to ", i)
+    # print(i)
+
+# powtarzanie czynnosci
+
+path = "c;//pawel/logi/log"
+
+#log1, log2, log3, ... log100
+
+# for i in range(0,100):
+#     new_path = path+str(i)
+#
+#     print(new_path)
+
+#zad range(6, 100, 6): #start 2, end = 100(bez 100), krok = 5
+
+# korzystajac z wyzej wymienionej konstrukcji oraz petli for wypisac liczby z zakresu od 200 do 950(bez 950)
+# z krokiem co 4 elementy
+
+
+# for i in range(200,950, 4):
+#     print("Wartość iteratora to " + str(i))
+
+# a = 200
+#
+# while(a<950):
+#     print(a)
+#     a+=4
+
+# moja_liczba = 9
+#
+# while(moja_liczba>0):
+#     print(moja_liczba)
+#     moja_liczba = int(input("Podaj liczbe "))
+
+a = 0
+
+# while(a<20):
+#     a+=1
+#     if a%2==0: #jesli liczba jest parzysta, to przeskocz ta sytuacje
+#         continue
+#     print(a)
+
+
+
+# while(a<20):
+#     a+=1
+#     if a%2==0: #jesli liczba jest parzysta, to koncz
+#         break
+#     print(a)
+
+
+#uzywajac petli while, wypisac kwadraty liczby 2, zakres od 1 do 1024
+
+# a = 200
+#
+# while(a<950):
+#     print(a)
+#     a+=4
+
+# a=1
+# while (a<=1024):
+#     print(a)
+#     a=a*2
+#
+
+s1 = "Ala ma kota"
+
+# print(s1[1:]) # wypisujemy s1 od drugiego elementu(z indeksem numer1)
+
+# print(s1[1:4]) # wypisujemy s1 od drugiego elementu(z indeksem numer1) do czwartego
+
+# print(s1[-1]) #ostatni element
+
+# print(s1[-2]) #przedostatni element
+
+# print(s1[-3]) #3 od konca
+
+# print(s1[-3]+" "+s1[-7]) konretnie 2 symbole(index=-3, oraz index=-7)
+
+# print(s1[-3]) #3 od konca
+
+
+# print(s1[3:7])
+
+# print(s1[::-1]) #odwrocenie stringu
+
+
+#pobieranie co ktoregos elementu
+
+# print(s1[0::2]) #pobierz co drugi element, zaczynajac od indeksu 0
+
+# print(s1[1::3]) #pobierz co trzeci element, zaczynajac od indeksu 1
+
+#liczymy liczbe znakow w danym stringu
+# print("liczba spacji w s1", s1.count(" "))
+
+# print('liczba spacji w s1', s1.count(' '))
+
+# print("liczba liter a w s1", s1.count("a"))
+
+# print("liczba literek a w s1 bez wzgledu na wielkosc litery", s1.lower().count('a'))
+
+# print("liczba liter a w s1", s1.count("a")+s1.count("A"))
+
+
+# print("Wszystkie duze litery", s1.upper())
+
+
+#laczenie slow(stringow)
+# ala = ["Ala", "ma", "kota"]
+# print("+".join(ala)) #laczymy slowa w pudelku ala za pomoca znaku +
+
+#dzielenie stringa
+
+# podzielony_string = s1.split(" ") #podzielony string wg spacji
+
+# s2 = "Ala ma kota       "
+# s3 = "       Ala ma kota"
+#
+# s2.strip()#golenie z dwoch stron
+# s3.lstrip()#golenie z lewej strony
+# s2.rstrip()#golenie z prawej strony
+
+# s1.replace("A", "X")#podmiana litery A na litere X
+
 
