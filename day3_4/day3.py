6297fac0b4955698a0c8b31501025198b27d83e7
Pawel <poczta130@gmail.com>
2022-01-19 09:38:27 +0100

    Update day3.py

--- day3_4/day3.py
+++ day3_4/day3.py
@@ -1,5 +1,6 @@
 import random
 
+#rozwiazanie zadania
 #chce pobrac pierwszy elemt items
 dane=open("plik.txt", "r").readlines()
 lista_ip=[]
@@ -19,3 +20,27 @@ for i, haslo in enumerate(lista_hasla):
         haslo=haslo+string
     lista_hasla[i]=haslo
 
+
+# with open("hasla.txt", "w") as f:
+#     for element in lista_hasla:
+#         f.write(str(element)+'\n') #kazdy element listy w nowym wierszu
+
+
+#ponownie skleic za pomoca znaku = lista_ip z lista_hasla, mozna uzyc petli for, oraz wynik do nowej listy
+# lista_ip[0]+"="+lista_hasla[0]
+
+lista_sklejona =[]
+
+for i in range(len(lista_hasla)):
+    pomocniczy=(lista_ip[i]+'='+lista_hasla[i]) #rownowaznie "=".join([lista_ip[0],lista_hasla[0]])
+
+
+    lista_sklejona.append(pomocniczy)
+
+with open("hasla2.txt", "w") as f:
+    for element in lista_sklejona:
+        f.write(str(element) + "\n")
+        
+        
+        
+ #funckje
