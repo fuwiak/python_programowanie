bb4af223a64023c225c8cb3635139c964b9e75cc
Pawel <poczta130@gmail.com>
2022-01-19 10:05:16 +0100

    Update day3.py

--- day3_4/day3.py
+++ day3_4/day3.py
@@ -1,6 +1,5 @@
 import random
 
-#rozwiazanie zadania
 #chce pobrac pierwszy elemt items
 dane=open("plik.txt", "r").readlines()
 lista_ip=[]
@@ -40,7 +39,49 @@ for i in range(len(lista_hasla)):
 with open("hasla2.txt", "w") as f:
     for element in lista_sklejona:
         f.write(str(element) + "\n")
-        
-        
-        
- #funckje
+
+
+#funkcje
+
+def kwadrat(x):
+    x = x**2
+
+    return x
+
+def kwadrat2(x):
+    x = x**2
+
+
+def delta(a,b,c):
+    d = b**2-4*a*c
+    if d>=0:
+        return "co najmniej jedno rozwiazanie"
+    else:
+        return "brak rozwiazan"
+
+# a,b,c = 1,2,1
+# print(delta(a,b,c))
+
+
+def powieksz_o_jeden(a,b,c):
+    a+=1
+    b+=1
+    c+=1
+    return a,b,c
+
+
+
+#zad1 napisac funkcje, ktora znaduje wieksza z dwoch podanych argumentow
+
+#zad2, to samo, tylko dla 3 argumentow
+
+
+
+
+
+
+
+
+
+
+
