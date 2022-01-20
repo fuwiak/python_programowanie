68ce13c3f22918a0b4c22775f0dd5fe9bae59beb
Pawel <poczta130@gmail.com>
2022-01-20 08:09:32 +0100

    Create nowy_log.py

---
+++ dane/nowy_log.py
@@ -0,0 +1,18 @@
+127.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+127.222.333.123 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Chrome/4.01 (Macintosh; I; PPC)"
+111.222.333.123 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 200 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+111.444.666.123 AWAY - [01/Feb/1998:01:09:14 -0800] "POST /bannerad/click.htm HTTP/1.0" 200 207 "http://www.referrer.com/bannerad/menu.htm" "Firefox/4.01 (Macintosh; I; PPC)"
+127.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+
+127.222.777.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Windows; I; PPC)"
+127.222.333.123 HOME - [01/Feb/1998:01:08:46 -0800] "POST /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Chrome/4.01 (Macintosh; I; PPC)"
+111.222.333.124 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 200 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+111.444.666.123 AWAY - [01/Feb/1998:01:09:14 -0800] "GET /bannerad/click.htm HTTP/1.0" 200 207 "http://www.referrer.com/bannerad/menu.htm" "Firefox/4.01 (Macintosh; I; PPC)"
+128.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "POST /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Windows); I; PPC
+
+127.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Ubuntu; I; PPC)"
+127.222.333.123 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Chrome/4.01 (Macintosh; I; PPC)"
+111.222.333.999 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 200 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Ubuntu; I; PPC)"
+111.444.666.123 AWAY - [01/Feb/1998:01:09:14 -0800] "POST /bannerad/click.htm HTTP/1.0" 200 207 "http://www.referrer.com/bannerad/menu.htm" "Firefox/4.01 (Macintosh; I; PPC)"
+127.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "https://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC
+
