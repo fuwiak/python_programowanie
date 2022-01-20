8dd8b5b3a97684023af7dd74528c64bf956f0d8f
Pawel <poczta130@gmail.com>
2022-01-20 10:22:45 +0100

    Create flatten_list.py

---
+++ day3_4/flatten_list.py
@@ -0,0 +1,10 @@
+
+# list_of_list = [['127.222.333.123'], ['127.222.333.123'], ['111.222.333.123']]
+#
+# #stuczka jak splaszczyc liste list
+#
+# fiatten = sum(list_of_list, [])
+# unique = set(fiatten)
+#
+#
+
