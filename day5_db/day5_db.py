cda630996410db8995eeb3b4661b106321be6aa9
Pawel <poczta130@gmail.com>
2022-01-21 08:48:16 +0100

    Update day5_db.py

--- day5_db/day5_db.py
+++ day5_db/day5_db.py
@@ -1,9 +1,19 @@
-create user jan with encrypted password 'jan';
+# create user jan with encrypted password 'jan';
 
-create database test22;
+# create database test22;
 
-\l   ---> sprawdzanie listy dostepnych baz danych
+# \l   ---> sprawdzanie listy dostepnych baz danych
 
-\du+ ---> lista dostepnych uzytkownikow
+# \du+ ---> lista dostepnych uzytkownikow
 
-grant all privileges on database test22 to jan;
+# grant all privileges on database test22 to jan;
+
+import psycopg2
+
+conn_data = {
+    'user': 'jan',
+    'host': '127.0.0.1',
+    'port':"5432",
+    'database': 'test22',
+    'password': 'pawel123',
+}
