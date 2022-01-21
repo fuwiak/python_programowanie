a718850526e398d27199f656358c9d6cd65f3fe6
Pawel <poczta130@gmail.com>
2022-01-21 09:17:06 +0100

    Update day5_db.py

--- day5_db/day5_db.py
+++ day5_db/day5_db.py
@@ -1,35 +1,49 @@
-# create user jan with encrypted password 'jan';
+mport psycopg2
+import random
 
-# create database test22;
+from my_functions import bulkInsert, deleteData, updateTable
 
-# \l   ---> sprawdzanie listy dostepnych baz danych
+from psycopg2 import Error
 
-# \du+ ---> lista dostepnych uzytkownikow
+# create user janusz with encrypted password 'janusz';
+# grant all privileges on database sample_db to user_name;
 
-# grant all privileges on database test22 to jan;
+#\du+
 
-import psycopg2
+# \create database test3;
 
-#dane do logowania
+
+#Polacz sie z baza danych
+# conn_data = {
+#     'user': 'postgres',
+#     'host': '127.0.0.1',
+#     'port':"5432",
+#     'database': 'postgres',
+#     'password': 'pawel123',
+# }
+
+
 conn_data = {
     'user': 'jan',
     'host': '127.0.0.1',
     'port':"5432",
     'database': 'test22',
-    'password': 'pawel123',
+    'password': 'jan',
 }
+
 
 connection = psycopg2.connect(**conn_data)
 
+#
 #create table(C)
 cursor = connection.cursor()
-create_table_query = '''CREATE TABLE IF NOT EXISTS mobile2
+create_table_query = '''CREATE TABLE IF NOT EXISTS mobile3
           (ID INT PRIMARY KEY     NOT NULL,
           MODEL           TEXT    NOT NULL,
           PRICE         FLOAT); '''
-
-
 # # Execute a command: this creates a new table
 cursor.execute(create_table_query)
 connection.commit()
+
+print("Potwierdzenie, ze prawidlowo polaczylismy sie z baza danych")
 print("Table created successfully in PostgreSQL ")
